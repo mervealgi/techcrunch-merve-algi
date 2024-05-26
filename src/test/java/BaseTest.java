28113961c2b93b549153765ce159946d5ec05800
mervealgi <mervealgi@gmail.com>
2024-05-26 23:58:24 +0300

    basetest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,19 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class BaseTest {
 
     WebDriver driver;
 
+    @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "/Users/mervealgi/IdeaProjects/techcrunch-merve-algi/chromedriver");
         driver = new ChromeDriver();
@@ -12,6 +21,26 @@ public class BaseTest {
 
     }
 
+    @Test
+    public void test() {
+        driver.get("https://techcrunch.com/");
+
+        // Find the 'The Latest' section
+        WebElement theLatest = driver.findElement(By.xpath("(//div[@class='wp-block-group is-layout-flow wp-block-group-is-layout-flow'])[1]"));
+        List<WebElement> articles = theLatest.findElements(By.xpath("//div[@class='wp-block-tc23-post-picker-group rapid-read-enabled rapid-read-date']"));
+
+        for (WebElement article : articles) {
+            // Check if the image exists
+            WebElement image = article.findElement(By.xpath(".//figure[contains(@class, 'post-block__media')]//img"));
+            Assert.assertTrue(image.isDisplayed(), "Image is not displayed for an article");
+
+            // Check if the title exists
+            WebElement title = article.findElement(By.xpath(".//header[contains(@class, 'post-block__header')]//h2"));
+            Assert.assertTrue(title.isDisplayed(), "Title is not displayed for an article");
+        }
+    }
+
+    @AfterClass
     public void tearDown() {
         if (driver != null) {
             driver.quit();
